804203beae392721af5b1df6497394a942cdd533
Brennan <brennan.watt@anza.xyz>
2025-05-15 21:56:47 +0000

    disable replay cost tracker

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -436,7 +436,7 @@ fn rebatch_and_execute_batches(
 
     if bank
         .feature_set
-        .is_active(&feature_set::apply_cost_tracker_during_replay::id())
+        .is_active(&feature_set::apply_cost_tracker_during_replay::id()) && false
     {
         let mut cost_tracker = bank.write_cost_tracker().unwrap();
         for tx_cost in &tx_costs {
