eb8d2d9c61bcb9ae7ed39decdfaa736f4a8492c5
Brennan <brennan.watt@anza.xyz>
2025-07-11 15:45:41 +0000

    300ms slots

--- poh/src/poh_service.rs
+++ poh/src/poh_service.rs
@@ -152,15 +152,16 @@ impl PohService {
         Self { tick_producer }
     }
 
-    pub fn target_ns_per_tick(ticks_per_slot: u64, target_tick_duration_ns: u64) -> u64 {
+    pub fn target_ns_per_tick(_ticks_per_slot: u64, _target_tick_duration_ns: u64) -> u64 {
+        return 300 * 1000 * 1000 - TARGET_SLOT_ADJUSTMENT_NS;
         // Account for some extra time outside of PoH generation to account
         // for processing time outside PoH.
-        let adjustment_per_tick = if ticks_per_slot > 0 {
+        /*let adjustment_per_tick = if ticks_per_slot > 0 {
             TARGET_SLOT_ADJUSTMENT_NS / ticks_per_slot
         } else {
             0
         };
-        target_tick_duration_ns.saturating_sub(adjustment_per_tick)
+        target_tick_duration_ns.saturating_sub(adjustment_per_tick)*/
     }
 
     fn low_power_tick_producer(
